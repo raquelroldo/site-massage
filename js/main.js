9ace44e4b136fb883a7df4d9d3f0a83c2528991e
raquelroldo <raquelroldo@gmail.com>
2019-06-24 17:39:03 +0100

    Created new contact  file with form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 (function (){
-	var btn = document.querySelector('#btn');
-	var menuList = document.querySelector('nav');
+	let btn = document.querySelector('.btn-menu');
+	let menuList = document.querySelector('nav');
+	let btnClose = document.querySelector('.btn-close');
 	btn.addEventListener('click', showMenu);
+	btnClose.addEventListener('click', showMenu);
 
 	function showMenu(){
 		if(menuList.classList.contains('active')){
