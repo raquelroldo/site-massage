94579ee6f825f7c88a94aa22b7a2189dde7e8ce4
raquelroldo <raquelroldo@gmail.com>
2019-07-12 11:45:10 +0100

    Added contact form. Started php integration.

---
+++ form.php
@@ -0,0 +1,10 @@
+<?php 
+    $nome = $_REQUEST['nome'];
+    $email = $_REQUEST['email'];
+    $newsletter = $_REQUEST['newsletter'];
+    $phone = $_REQUEST['phone'];
+    $msg = $_REQUEST['mensagem'];
+
+    echo('Nome: '. $nome);
+
+?>
--- js/main.js
+++ js/main.js
@@ -12,4 +12,5 @@
 			menuList.classList.add('active');	
 		}
 	}
+
 })()
